9ebf189882f72ad40a199a9a8a347137c1fede80
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-06 11:18:16 +0100

    updated App.js with components for EvenClicks and CountBy challenges

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Multiplier from "./Multiplier";
+import EvenClicks from "./EvenClicks";
+import CountBy from "./CountBy";
 
 let App = () => {
 	return (
 		<>
-			<Multiplier x={ 10 } y={ 20 }/>
+			<Multiplier x={ 10 } y={ 20 } />
+			<EvenClicks />
+			<CountBy step={ 5 } />
 		</>
 	);
 }
