3f18bc434908f9f3bc7cd27faf0fbba14f60d377
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-06 12:37:46 +0100

    updated App.js with routes for Multiplier, EvenClicks and CountBy challenges

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
+
+import {
+	BrowserRouter as Router,
+	Route,
+} from "react-router-dom";
+
 import Multiplier from "./Multiplier";
 import EvenClicks from "./EvenClicks";
 import CountBy from "./CountBy";
 import HideMe from "./HideMe";
 import MinimumLength from "./MinimumLength";
 
-let App = () => {
+const App = () => {
 	return (
-		<>
-			<Multiplier x={ 10 } y={ 20 } />
-			<EvenClicks />
-			<CountBy step={ 5 } />
-			<HideMe>Blah blah blah</HideMe>
-			<MinimumLength length={ 30 } />
-		</>
+		<Router>
+			<Fragment>
+				<Route exact path="/multiplier/:x/:y" render={ ({ match }) => (
+					<Multiplier x={ match.params.x } y={ match.params.y } />
+				) } />
+				<Route exact path="/evenclicks" component={ EvenClicks } />
+				<Route exact path="/count-by/:step" render={ ({ match }) => (
+					<CountBy step={ match.params.step } />
+				) } />
+				<HideMe>Blah blah blah</HideMe>
+				<MinimumLength length={ 30 } />
+			</Fragment>
+		</Router>
 	);
 }
 
