7ae5ae03233ec5c018429e76a556b2c93bef8640
Jonathan Smithies <jonathan@elev8now.co.uk>
2019-09-06 12:51:32 +0100

    fixed a bug in MinimumLength challenge where input length message would show negative numbers

--- src/MinimumLength.js
+++ src/MinimumLength.js
@@ -3,27 +3,30 @@ import React, { Component } from "react";
 class MinimumLength extends Component {
     constructor(props) {
         super(props);
-        this.state = { input: "", message: "" };
+        this.state = { input: "" };
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e) {
-        let { length } = this.props;
         let inputEl = e.currentTarget.value
         this.setState({
             input: inputEl,
-            message: `${length - inputEl.length} characters required`
         })
     }
 
     render() {
+        let { length } = this.props;
         let { input, message } = this.state;
         return (
             <div className="jumbotron">
                 <h4>Minimum Length</h4>
                 <form>
                     <input onChange={ this.handleChange } value={ input } className="form-control" />
-                    <label className="mt-2">{ message }</label>
+                    <label className="mt-2">
+                        {
+                            input.length >= 0 && input.length < length ? `${length - input.length} characters required` : ""
+                        }
+                    </label>
                 </form>
             </div>
         )
